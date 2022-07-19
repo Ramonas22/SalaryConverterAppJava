580d2162c8ebca9cc63bbdb868bb7a17cfd99279
Ramonas <sanomaras23@gmail.com>
2022-07-19 18:56:10 +0300

    Removed partially working NPD feature

--- app/src/main/java/com/renvictus/salarycalculator/calculations/SalaryConverter.java
+++ app/src/main/java/com/renvictus/salarycalculator/calculations/SalaryConverter.java
@@ -5,27 +5,29 @@ public class SalaryConverter {
     public static Double grossIncomeToNet(Double salaryGross) {
 
         //Darbuotojo sumokami mokesciai
-        double employeeTaxes = (salaryGross - CalculateNPD.grossNPD(salaryGross)) * Const.INCOME_TAX
+        //double employeeTaxes = (salaryGross - CalculateNPD.grossNPD(salaryGross))
+        double employeeTaxes = salaryGross * Const.INCOME_TAX
                 + salaryGross * (
                 Const.PENSION_INSURANCE
                         + Const.MANDATORY_HEALTH_INSURANCE
                         + Const.ILLNESS_SOCIAL_INSURANCE
                         + Const.MOTHERHOOD_SOCIAL_INSURANCE);
 
-        return salaryGross - employeeTaxes;
+        return (double) (Math.round((salaryGross - employeeTaxes) * 100) / 100);
     }
 
     public static Double netIncomeToGross(Double salaryNet) {
 
         //Darbuotojo sumokami mokesciai
-        double employeeTaxes = (salaryNet - CalculateNPD.netNPD(salaryNet)) * Const.INCOME_TAX
-                + salaryNet * (
-                Const.PENSION_INSURANCE
-                        + Const.MANDATORY_HEALTH_INSURANCE
-                        + Const.ILLNESS_SOCIAL_INSURANCE
-                        + Const.MOTHERHOOD_SOCIAL_INSURANCE);
+        //double employeeTaxes = (salaryNet - CalculateNPD.netNPD(salaryNet))
+//        double employeeTaxes = salaryNet * Const.INCOME_TAX
+//                + salaryNet * (
+//                Const.PENSION_INSURANCE
+//                        + Const.MANDATORY_HEALTH_INSURANCE
+//                        + Const.ILLNESS_SOCIAL_INSURANCE
+//                        + Const.MOTHERHOOD_SOCIAL_INSURANCE);
 
         //Darbuotojo sumokami mokesciai
-        return salaryNet * salaryNet / grossIncomeToNet(salaryNet);
+        return (double) Math.round((salaryNet * salaryNet / grossIncomeToNet(salaryNet)) * 100) / 100;
     }
 }
